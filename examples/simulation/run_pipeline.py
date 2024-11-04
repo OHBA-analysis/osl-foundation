ecb63efab075803a43d4d7fe48d9377b4ac763aa
Rukuang Huang <rukuang.huang@jesus.ox.ac.uk>
2024-11-04 10:25:28 +0000

    Fix: Bug in submit file.

---
+++ examples/simulation/run_pipeline.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("python 1_simulate_data.py")
+os.system("python 2_train_tokenizer.py")
+os.system("python 3_train_generator.py")
+os.system("python 4_generate_data.py")
--- examples/simulation/submit.py
+++ examples/simulation/submit.py
@@ -5,17 +5,14 @@ import os
 
 def write_and_submit_job_script():
     with open("job.sh", "w") as file:
-        name = f"{script}-{run}"
+        name = "simulation_example"
         file.write("#!/bin/bash\n")
         file.write(f"#SBATCH -J {name}\n")
         file.write(f"#SBATCH -o outputs/{name}.out\n")
         file.write("#SBATCH -p gpu_short\n")
         file.write(f"#SBATCH --gres gpu:1 --constraint 'a100|v100|rtx8000'\n")
         file.write("source activate osld\n")
-        file.write(f"python 1_simulate_data.py\n")
-        file.write(f"python 2_train_tokenizer.py\n")
-        file.write(f"python 3_train_generator.py\n")
-        file.write(f"python 4_generate_data.py\n")
+        file.write(f"python run_pipeline.py\n")
 
     os.system("sbatch job.sh")
     os.system("rm job.sh")
