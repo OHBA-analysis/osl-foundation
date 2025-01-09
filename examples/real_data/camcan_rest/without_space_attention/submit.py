e5041b0900bb948c31ece8e284639bc1f90cf254
Rukuang Huang <rukuang.huang@jesus.ox.ac.uk>
2025-01-09 16:57:52 +0000

    Feat: Submit dependent jobs.

---
+++ examples/real_data/camcan_rest/without_space_attention/submit.py
@@ -0,0 +1,26 @@
+"""Script for submitting training jobs to the BMRC cluster."""
+
+import os
+
+
+def write_job_script(after: str = None, run: int = 0):
+    with open("job.sh", "w") as file:
+        name = f"ex_{run}"
+        file.write("#!/bin/bash\n")
+        file.write(f"#SBATCH -J {name}\n")
+        file.write(f"#SBATCH -o outputs/{name}.out\n")
+        file.write("#SBATCH -p gpu_long\n")
+        file.write(f"#SBATCH --gres gpu:1\n")
+        file.write("#SBATCH --parsable\n")
+        file.write("source activate osld\n")
+        if after:
+            file.write(f"#SBATCH -d afterok:{after}\n")
+        file.write(f"python 3_train_generator.py\n")
+
+
+os.makedirs("outputs", exist_ok=True)
+job_id = None
+for i in range(30):
+    write_job_script(after=job_id, run=i)
+    job_id = str(int(os.popen("sbatch job.sh").read()))
+    os.system("rm job.sh")
