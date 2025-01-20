4fa4a238f3b277189005e3537bc049045a0bc358
scho97 <sungjun.cho@ndcn.ox.ac.uk>
2025-01-20 22:00:30 +0000

    enhance: option to save config when creating model

--- examples/simulation/with_space_attention/3_train_generator.py
+++ examples/simulation/with_space_attention/3_train_generator.py
@@ -62,8 +62,7 @@ generator_config = f"""
             verbose: 1
 """
 
-generator = create_model(generator_config)
-generator.save_config(generator_dir)  # TODO: integrate this into create_model
+generator = create_model(generator_config, save_dir=generator_dir)
 generator.summary()
 
 # ---------- Fit generator ---------- #
