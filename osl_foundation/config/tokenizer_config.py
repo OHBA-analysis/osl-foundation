79d6d7282da1dad7323128bcc01e75be07a4b721
SungJun Cho <sungjun.cho@ndcn.ox.ac.uk>
2025-03-17 22:59:33 +0900

    add: causal tokenizer functionality (#28)

--- osl_foundation/config/tokenizer_config.py
+++ osl_foundation/config/tokenizer_config.py
@@ -10,6 +10,7 @@ class OSLTokenizerModelConfig(BaseModelConfig):
     name: str = "osl_tokenizer"
     n_tokens: int = None
     token_dim: int = None
+    token_kernel_padding: str = None
     rnn_n_units: int = None
     rnn_type: str = None
     rnn_n_layers: int = None
@@ -18,6 +19,7 @@ class OSLTokenizerModelConfig(BaseModelConfig):
         super().validate()
         assert self.n_tokens is not None, "n_tokens must be set"
         assert self.token_dim is not None, "token_dim must be set"
+        assert self.token_kernel_padding is not None, "token_kernel_padding must be set"
         assert self.rnn_n_units is not None, "rnn_n_units must be set"
         assert self.rnn_type is not None, "rnn_type must be set"
         assert self.rnn_n_layers is not None, "rnn_n_layers must be set"
@@ -28,6 +30,7 @@ class OSLTokenizerModelConfig(BaseModelConfig):
     def set_config(self, config: dict) -> None:
         self.n_tokens = config.get("n_tokens", 16)
         self.token_dim = config.get("token_dim", 32)
+        self.token_kernel_padding = config.get("token_kernel_padding", "same")
         self.rnn_n_units = config.get("rnn_n_units", 64)
         self.rnn_type = config.get("rnn_type", "gru")
         self.rnn_n_layers = config.get("rnn_n_layers", 1)
